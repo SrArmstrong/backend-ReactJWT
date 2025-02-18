52453d9adfaecc770f928a69d0814b994d5aedc0
SrArmstrong <cetisaldavalde@gmail.com>
2025-02-18 00:39:53 -0600

    subiendo archivos

---
+++ src/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+const users = [ // Usuarios registrados para uso del programa
+  { username: 'admin', password: 'password' },
+  { username: 'sergio', password: 'sergio' }
+];
+
+const authenticateUser = (username, password) => { // Consulta de usuarios registrados 
+  return users.find(user => user.username === username && user.password === password);
+};
+
+const generateToken = (username) => { // Generación de TOKEN para usuarios JWT
+    console.log("JWT_SECRET cargado:", process.env.JWT_SECRET); // Respuesta en la terminal cuando cargue un usuario (TOKEN)
+    return jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1m' }); // Duración del TOKEN (1 Minuto)
+};
+
+module.exports = { authenticateUser, generateToken };
